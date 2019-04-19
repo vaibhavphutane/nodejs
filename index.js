a72eefac7e7698b3468914365d1348d61bd0930a
xtoolNH <xtoolnihilent@gmail.com>
2019-04-19 17:00:14 +0530

    Connection string encoding

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-// const morganStreamLog = fs.createWriteStream(path.join(__dirname, 'logs/app.log'), {flags: 'a'});
+const morganStreamLog = fs.createWriteStream(path.join(__dirname, 'logs/app.log'), {flags: 'a'});
 
 app.use(cors());
 app.use((req, res, next) => {
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.json());
-// app.use(morgan('combined', {stream: morganStreamLog}));
+app.use(morgan('combined', {stream: morganStreamLog}));
 app.use('/api/users', users);
 
 const PORT = process.env.PORT || 3000;
